8dfae707c9e83e9c39ba713695457f202b99e40e
Valerii Shymchuk <valera01092006@gmail.com>
2026-04-15 09:46:32 +0300

    Fixed issue with units selection in city.

--- core/src/com/unciv/ui/components/input/ActivationListener.kt
+++ core/src/com/unciv/ui/components/input/ActivationListener.kt
@@ -7,6 +7,14 @@ import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener
 class ActivationListener : ActorGestureListener(20f, 0.25f, 1.1f, Int.MAX_VALUE.toFloat()) {
     // defaults are: halfTapSquareSize = 20, tapCountInterval = 0.4f, longPressDuration = 1.1f, maxFlingDelay = Integer.MAX_VALUE
 
+    override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int) {
+        event?.listenerActor ?: return
+        ActivationTypes.entries.firstOrNull {
+            it.isGesture && it.button == button
+        } ?: return
+        event.stop()
+    }
+    
     override fun tap(event: InputEvent?, x: Float, y: Float, count: Int, button: Int) {
         val actor = event?.listenerActor ?: return
         val type = ActivationTypes.entries.firstOrNull {
